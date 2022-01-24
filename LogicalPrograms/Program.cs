abf8808a03cc75d30d6ccb8ddd2ff8266bf6ba8b
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-24 22:37:31 +0530

    [Shreya] UC1_fibonacciSeries

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LogicalPrograms;
+Console.WriteLine("Hello, World!");
+FibonacciSeries series = new FibonacciSeries();
+series.findFibonacciSeries();
